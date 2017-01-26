5362a56df2f14f192862aa46b74693011f0e1aec
andigan <andrew.nease.code@gmail.com>
2017-01-26 17:05:41 -0500

    add board

--- js/index.js
+++ js/index.js
@@ -3,44 +3,47 @@ $(document).ready(function() {
     // $('#wrapper').fadeOut(500)
     // $('#wrapper').fadeIn(500)
 
-    // get window size
-    var mainWidth = window.innerWidth
-    var mainHeight = window.innerHeight
 
-    // set size
-    document.getElementById('wrapper').style.width = mainWidth + 'px'
-    document.getElementById('wrapper').style.height = mainHeight + 'px'
-
-    // get true pixel board size based the percentage width of actual window
-    var boardElement = document.getElementById('board')
-    var computedBoardElementStyle = window.getComputedStyle(boardElement, null)
-
-    var boardWidth = parseInt(computedBoardElementStyle.width)
-    var boardHeight = parseInt(computedBoardElementStyle.height)
+    // configuration settings
 
     // number of rows & columns the board will have
     var columns = 5
     var rows = 5
 
-    // set cell board width & height
+
+    // variable declarations
+
+    // store the total number of cells for the SetUpBoard for loop
+    var totalCells = columns * rows
+
+    // get window size (doing it this way will help with cell phone browsers)
+    var mainWidth = window.innerWidth
+    var mainHeight = window.innerHeight
+
+    // set wrapper size to the window size
+    document.getElementById('wrapper').style.width = mainWidth + 'px'
+    document.getElementById('wrapper').style.height = mainHeight + 'px'
+
+    // get the true pixel size of the board, which was set using percentage of view in the #board CSS
+    var boardElement = document.getElementById('board')
+    var computedBoardElementStyle = window.getComputedStyle(boardElement, null)
+
+    // store the computed style (true pixel count) of the board
+    var boardWidth = parseInt(computedBoardElementStyle.width)
+    var boardHeight = parseInt(computedBoardElementStyle.height)
+
+    // set the cells' width & height
     var cellBoxWidth = boardWidth / columns
     var cellBoxHeight = boardHeight / rows
 
-    // function setUpBoard (columns, rows) {
-    //   for (var i = 0; i < columns; i++) {
-    //     for (var i = 0; i < rows; i++) {
-    //        createCell()
-    //     }
-    //   }
-    // }
 
 
 
-    function createCell(position, uniqueCellId) {
+    // define functions
+
+    function createCell(position, iterator) {
       var boardElement = document.getElementById('board')
       var cellElement = document.createElement('div')
-      console.log('test')
-      console.log(document.getElementById('wrapper').style.width)
 
       cellElement.setAttribute('id', position)
 
@@ -48,31 +51,47 @@ $(document).ready(function() {
 
       cellElement.style.width = cellBoxWidth + 'px'
       cellElement.style.height = cellBoxHeight + 'px'
-      cellElement.style.backgroundColor = 'grey'
-      // cellElement.style.top = '10px'
-      // cellElement.style.left = '10px'
+//      cellElement.style.backgroundColor = 'grey'
 
-      console.log('cellboxHeight: ' + cellBoxHeight)
-      console.log('board width: ' + boardWidth)
-      console.log('id: ' + uniqueCellId)
-      console.log('id % rows: ' + uniqueCellId % rows)
-      console.log('id % rows * cellboxHeight: ' + (uniqueCellId % rows) * cellBoxWidth)
+      // this is to demonstrate and display the id of the div
+      cellElement.textContent = position.toString()
+
+      var toprow = (iterator % rows === 0)
+      var bottomrow = (iterator % rows === (rows - 1))
+      var leftrow = (Math.floor(iterator / rows) === 0)
+      var rightrow = (Math.floor(iterator / rows) === (columns - 1))
+
+      if (toprow) {cellElement.classList.add('topRow')}
+      if (bottomrow) {cellElement.classList.add('bottomRow')}
+      if (leftrow) {cellElement.classList.add('leftRow')}
+      if (rightrow) {cellElement.classList.add('rightRow')}
 
 
+      // this is to demonstrate that the above booleans are reliable
+      // cellElement.textContent = rightrow
 
-
-
-
-      cellElement.style.top = (uniqueCellId % rows) * cellBoxHeight + 'px'
-      cellElement.style.left = (Math.floor(uniqueCellId / rows) * cellBoxWidth) + 'px'
+      // set the top and left coordinates of each div
+      cellElement.style.top = (iterator % rows) * cellBoxHeight + 'px'
+      cellElement.style.left = (Math.floor(iterator / rows) * cellBoxWidth) + 'px'
 
       boardElement.appendChild(cellElement)
-
     }
 
-    for (var i = 0; i < 25; i++) {
-        createCell(i, i)
+
+
+    function SetUpBoard() {
+      for (var i = 0; i < totalCells; i++) {
+
+        // createCell accepts (id, iterator)
+        // id will be the position on the grid
+        // y coordinate = rows - (i % rows)
+        // x coordinate = Math.floor(i / rows)
+        createCell(`[${1 + (Math.floor(i / rows))}, ${rows - (i % rows)}]`, i)
+
+      }
     }
+
+    SetUpBoard()
 
 
 })
