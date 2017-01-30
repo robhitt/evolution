746278d94831ff1a2f7248a49b94dc1670b947ff
Rob Hitt <robhitt@gmail.com>
2017-01-29 19:59:00 -0500

    gradient and console log

--- js/index.js
+++ js/index.js
@@ -15,6 +15,34 @@ $(document).ready(function() {
   var game = new Evolution
   var mobile_movements = new MobileMovements
 
+console.log("==========")
+console.log("Made with care by: robhitt@gmail.com or andrew.nease.code@gmail.com")
+console.log("==========")
+console.log("            __")
+console.log("           / _)")
+console.log("    .-^^^-/ /")
+console.log("__/       /")
+console.log("<__.|_|-|_|")
+
+  //
+  //
+  // console.log("                            .       .")
+  // console.log("                         / `.   .' \\")
+  // console.log("                 .---.  <    > <    >  .---.")
+  // console.log("                 |    \  \ - ~ ~ - /  /    |")
+  // console.log("                  ~-..-~             ~-..-~")
+  // console.log("              \~~~\.'                    `./~~~/")
+  // console.log("               \__/                        \__/")
+  // console.log("                /                  .-    .  \\")
+  // console.log("         _._ _.-    .-~ ~-.       /       }   \/~~~/")
+  // console.log("     _.-'q  }~     /       }     {        ;    \__/")
+  // console.log("    {'__,  /      (       /      {       /      `. ,~~|   .     .")
+  // console.log("     `''''='~~-.__(      /_      |      /- _      `..-'   \\   //")
+  // console.log("                 / \   =/  ~~--~~{    ./|    ~-.     `-..__\\_//_.-'")
+  // console.log("                {   \  +\         \  =\ (        ~ - . _ _ _..---~")
+  // console.log("                |  | {   }         \   \_\\")
+  // console.log("               '---.o___,'       .o___,'     ")
+
 
   // test mobile sound
 // document.getElementById('board').addEventListener('touchend', handleTouchEnd, false);
